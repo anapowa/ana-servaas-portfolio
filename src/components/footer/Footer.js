a2d685c3748719283a6f8c2285e971f602beba92
Ana Ruiz Tovar <anapowa@gmail.com>
2021-06-29 10:33:44 -0400

    Trying to silence alert in footer.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,10 +5,10 @@ export const Footer = () => {
         <footer>
                 <section id="contact">
                     <address>
-                        <a id="resume" className="icon-before icon-after" href="https://drive.google.com/file/d/1UFMJAPPb7sywsSB87f7SUKUZkQ6iqfzq/view?usp=sharing" title= "resume" alt="resume link"></a>. {/* eslint-disable-line no-alert */}
-                        <a id="github" className="icon-before icon-after" href="https://github.com/anapowa" title="github" alt="github"></a> // eslint-disable-line no-alert {/* eslint-disable-line no-alert */}
-                        <a id="linkedin" className="icon-before icon-after" href="https://www.linkedin.com/in/anapowa/" title="linkedin" alt="linkedin link"></a> // eslint-disable-line no-alert {/* eslint-disable-line no-alert */}
-                        <a id="twitter" className="icon-before icon-after" href="https://twitter.com/anapowa" title="twitter" alt="twitter link"></a> // eslint-disable-line no-alert {/* eslint-disable-line no-alert */}
+                        <a id="resume" className="icon-before icon-after" href="https://drive.google.com/file/d/1UFMJAPPb7sywsSB87f7SUKUZkQ6iqfzq/view?usp=sharing" title= "resume" alt="resume link"></a>. {/* //eslint-disable-line no-alert */}
+                        <a id="github" className="icon-before icon-after" href="https://github.com/anapowa" title="github" alt="github"></a> // eslint-disable-line no-alert {/* //eslint-disable-line no-alert */}
+                        <a id="linkedin" className="icon-before icon-after" href="https://www.linkedin.com/in/anapowa/" title="linkedin" alt="linkedin link"></a> // eslint-disable-line no-alert {/* //eslint-disable-line no-alert */}
+                        <a id="twitter" className="icon-before icon-after" href="https://twitter.com/anapowa" title="twitter" alt="twitter link"></a> // eslint-disable-line no-alert {/* //eslint-disable-line no-alert */}
                         <a id="mail" className="icon-before icon-after" href="mailto:ana.servaas@gmail.com" title="email" alt="mailto link"></a>. {/* eslint-disable-line no-alert */}
                     </address>    
                 </section>
